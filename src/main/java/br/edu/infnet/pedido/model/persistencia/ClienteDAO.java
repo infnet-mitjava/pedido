01cd59a40b6841693c7295a89804c4b05ac32e8c
edveloso <edveloso@gmail.com>
2022-10-24 17:24:38 -0300

    abstract dao

--- src/main/java/br/edu/infnet/pedido/model/persistencia/ClienteDAO.java
+++ src/main/java/br/edu/infnet/pedido/model/persistencia/ClienteDAO.java
@@ -1,29 +1,17 @@
 package br.edu.infnet.pedido.model.persistencia;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.edu.infnet.pedido.model.JdbcUtil;
 import br.edu.infnet.pedido.model.entidade.Cliente;
 
-public class ClienteDAO implements IDAO<Cliente> {
+public class ClienteDAO extends JdbcDAO<Cliente>  {
 
-	private Connection con;
-	private PreparedStatement pstm;
-	private Statement stm;
-	private ResultSet rs;
-	
 	public ClienteDAO() {
-		con = JdbcUtil.obterConexao();
 	}
 	
-	//CRUD
-	//CREATE RETRIEVE UPDATE DELETE
+
 	@Override
 	public Boolean salvar(Cliente cliente) {
 		String sql = "insert into cliente(nome, codigo) values (?,null)";
@@ -36,7 +24,7 @@ public class ClienteDAO implements IDAO<Cliente> {
 		}
 		return false;
 	}
-	
+
 	@Override
 	public Boolean atualizar(Cliente cliente) {
 		String sql = "update cliente values (?) where codigo = ?";
@@ -50,7 +38,7 @@ public class ClienteDAO implements IDAO<Cliente> {
 		}
 		return false;
 	}
-	
+
 	@Override
 	public Boolean deletar(Cliente cliente) {
 		String sql = "delete from cliente  where codigo = ?";
@@ -63,9 +51,9 @@ public class ClienteDAO implements IDAO<Cliente> {
 		}
 		return false;
 	}
-	
+
 	@Override
-	public List<Cliente> listarTodos(){
+	public List<Cliente> listarTodos() {
 		String sql = "select * from cliente";
 		List<Cliente> clientes = new ArrayList<>();
 		try {
@@ -83,9 +71,9 @@ public class ClienteDAO implements IDAO<Cliente> {
 		}
 		return null;
 	}
-	
+
 	@Override
-	public Cliente obter(Long codigo){
+	public Cliente obter(Long codigo) {
 		String sql = "select * from cliente where codigo = ? ";
 		try {
 			stm = con.createStatement();
@@ -98,5 +86,6 @@ public class ClienteDAO implements IDAO<Cliente> {
 		}
 		return null;
 	}
+
 	
 }
